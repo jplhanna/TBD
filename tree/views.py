6cfc15a054adda0855c782b85276fe967e5e12a2
Tristan Villamil <villamil101@gmail.com>
2017-02-02 20:07:12 +0000

    Added first sample page

--- tree/views.py
+++ tree/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponseRedirect, HttpResponse, HttpRequest
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, "index.html")
