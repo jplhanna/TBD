f5ad8e9e728a00d7244aa5335ac4bf544446ffa1
jplhanna <jpl.hanna@gmail.com>
2017-03-19 23:08:48 +0000

    Created a population command within for manage.py, as well as a parser for finding the important information from the movie csv. Code for this can be found in populate.py and admin.py in tree, and parser.py respectively

---
+++ parser.py
@@ -0,0 +1,21 @@
+import csv
+
+
+'''
+MovieParser: Method meant to parse through a csv file containing information scraped from imdb on films
+input: text_File: A string which holds the location of the csv file which is going to be parsed
+output: A list of tupples containing all of the important movie information which is in the csv
+'''
+def MovieParser(text_File):
+    _movie_List=[]
+    with open(text_File,'rb') as csvfile:
+        _csv_tmp=csv.reader(csvfile,delimiter=',')
+        #could change the code so that it finds the loction of the information using the first line of the csv using indexof
+        #this way we are not using magic numbers, and don't have to worry about a consistent csv format for the parser
+        for _row_tmp in _csv_tmp:
+            if(_row_tmp[1]=='director_name'):
+                continue
+            #3: duration 11: name 17: imdb link 23: year
+            _movie_tmp=(_row_tmp[3],_row_tmp[11],_row_tmp[17],_row_tmp[23])#figure out the positions of the values we need, write them down
+            _movie_List.append(_movie_tmp)
+    return _movie_List
---
+++ tree/management/commands/populate.py
@@ -0,0 +1,29 @@
+from django.core.management.base import BaseCommand
+from .models import Movie
+from parser import MovieParser
+
+#So rather than calling the parser we call manage with a specific function line which will call this. The function should include the location of the file being added
+#Parser could be called to parse the file and return the proper list. The parser could be a factor method
+
+class Command(BaseCommand):
+    
+    '''
+    CreateMovies: A method which populates or adds movies into the database
+    input: csv: A string containing the file location with the movies to be input
+    modifies: The movie database contained in db.splite3, and based on the Movie model in the Tree application. Adds movies into said database
+    '''
+    def CreateMovies(self, csv):
+        _movie_Data=MovieParser(csv)
+        for _movie_tmp in _movie_Data:
+            _new_Movie_tmp=Movie(_movie_tmp[1],_movie_tmp[2],_movie_tmp[0],_movie_tmp[3])
+            _new_Movie_tmp.save()
+        
+    '''
+    handle: The command connected to the manage.py command prompt. Is meant to update/populate the movie database using a csv
+    input: *args: A list of arguments which have been input. Which should just include the csv file location
+           **options: A map of operations, which contain options for the command which is being run. 
+    '''
+    def handle(self,*args,**options):
+        if(len(args)!=1):
+            raise IndexError('Incorrect number of arguments for command')
+        self.CreateMovies(options[0])
