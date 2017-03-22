c1cc8361163b68df8de4ec69008084f70e7d4165
Gin <bazinga931212@gmail.com>
2017-03-22 18:45:09 -0400

    add form.py for login

---
+++ tree/form.py
@@ -0,0 +1,32 @@
+from django import forms
+from django.contrib.auth.models import User
+from bootstrap_toolkit.widgets import BootstrapDateInput, BootstrapTextInput, BootstrapUneditableInput
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(
+        required=True,
+        label="Email",
+        error_messages={'required': 'Please enter email'},
+        widget=forms.TextInput(
+            attrs={
+                'placeholder': "Email",
+            }
+        ),
+    )
+    password = forms.CharField(
+        required=True,
+        label=u"密码",
+        error_messages={'required': 'Please enter the password'},
+        widget=forms.PasswordInput(
+            attrs={
+                'placeholder': "Password",
+            }
+        ),
+    )
+
+    def clean(self):
+        if not self.is_valid():
+            raise forms.ValidationError("Email and Password are required")
+        else:
+            cleaned_data = super(LoginForm, self).clean()
